b35efd2a5dbaf03f82ebbedc638c8db8b457dac7
Naveen <naveenmurugesan1972@gmail.com>
2026-02-12 09:44:49 +0530

    Add files via upload

---
+++ 3_Sequential/Flip_Flops/JK_flip_flop/async_negedgerst_jk_ff.v
@@ -0,0 +1,19 @@
+module jk_ff(
+input clk,rst,j,k,
+output reg q,
+output q_bar);
+always@(posedge clk or negedge rst)begin
+	if(!rst)
+		q<=1'b0;
+	else begin
+case({j,k})
+2'b00:q<=q;
+2'b01:q<=1'b0;
+2'b10:q<=1'b1;
+2'b11:q<=~q;
+default:q<=1'b0;
+endcase
+end
+end
+assign q_bar=~q;
+endmodule
