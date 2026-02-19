c6907ba639d17a67456775184fc134a59453fc97
Naveen <naveenmurugesan1972@gmail.com>
2026-02-19 09:46:44 +0530

    Update sync_piso_sr.v

--- 3_Sequential/Shift_Registers/PISO/sync_piso_sr.v
+++ 3_Sequential/Shift_Registers/PISO/sync_piso_sr.v
@@ -26,7 +26,7 @@ output out);
 wire [3:0]q,a,b;
 assign a=~in;
 assign b=~q;
-sr_ff jk1(.clk(clk), .rst(rst), .s(load?in[0]:q[0]), .r(load?a[0]:b[0]), .q(q[0]));
+sr_ff jk1(.clk(clk), .rst(rst), .s(load?in[0]:1'b0), .r(load?a[0]:1'b1), .q(q[0]));
 sr_ff jk2(.clk(clk), .rst(rst), .s(load?in[1]:q[0]), .r(load?a[1]:b[0]), .q(q[1]));
 sr_ff jk3(.clk(clk), .rst(rst), .s(load?in[2]:q[1]), .r(load?a[2]:b[1]), .q(q[2]));
 sr_ff jk4(.clk(clk), .rst(rst), .s(load?in[3]:q[2]), .r(load?a[3]:b[2]), .q(q[3]));
