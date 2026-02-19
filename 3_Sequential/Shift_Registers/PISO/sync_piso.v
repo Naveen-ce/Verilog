b3b382e6a36cedd78063370260c3a7683096dce8
Naveen <naveenmurugesan1972@gmail.com>
2026-02-19 09:44:39 +0530

    Update sync_piso.v

--- 3_Sequential/Shift_Registers/PISO/sync_piso.v
+++ 3_Sequential/Shift_Registers/PISO/sync_piso.v
@@ -5,12 +5,13 @@ output reg out);
 reg [3:0]q;
 always@(posedge clk or posedge rst)begin
 if(rst)
-q<=4'b0000;
+q=4'b0000;
 else if (load)
-q<=in;
-else
-q<=({in,q[3:1]});
-out<=q[0];
+q=in;
+else begin
+q=({in,q[3:1]});
+out=q[0];
+end
 end
 endmodule
 
