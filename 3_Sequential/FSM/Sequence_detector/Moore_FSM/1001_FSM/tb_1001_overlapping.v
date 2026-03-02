b40d9c1abd5e09da990a35477d8bfd7dfdeaab3f
Naveen <naveenmurugesan1972@gmail.com>
2026-03-02 12:06:59 +0530

    Add files via upload

---
+++ 3_Sequential/FSM/Sequence_detector/Moore_FSM/1001_FSM/tb_1001_overlapping.v
@@ -0,0 +1,27 @@
+module tb_moore_fsm;
+reg clk,rst,a;
+wire q;
+moore_fsm uut(.clk(clk), .rst(rst), .a(a), .q(q));
+always #5 clk=~clk;
+initial begin
+	clk=0; rst=1; a=0;
+	#5 rst=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=0;
+	#10 a=1;
+	#10 a=0;
+	#10 a=0;
+	#10 a=1;
+        #10 a=1;
+	#10 $finish;
+end
+initial begin
+	$monitor("Time=%0t | clk=%b | rst=%b | a=%b | q=%b",$time,clk,rst,a,q);
+	$dumpfile("over_moore_fsm");
+	$dumpvars(0,tb_moore_fsm);
+end
+endmodule
+
+
