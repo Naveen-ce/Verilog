1326fdf374b4454e7ffdc2e703f182cc3e0e278b
Naveen <naveenmurugesan1972@gmail.com>
2026-03-02 12:01:48 +0530

    Add files via upload

---
+++ 3_Sequential/FSM/Sequence_detector/Mealy_FSM/1011_FSM/tb_1011_overlapping.v
@@ -0,0 +1,28 @@
+module tb_mealy_fsm;
+reg clk,rst,a;
+wire q;
+mealy_fsm uut(.clk(clk), .rst(rst), .a(a), .q(q));
+always #5 clk=~clk;
+initial begin
+	clk=0; rst=1; a=0;
+	#5 rst=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=1;
+	#10 $finish;
+end
+initial begin
+	$monitor("Time=0%t | clk=%b | rst=%b | a=%b | q=%b",$time,clk,rst,a,q);
+	$dumpfile("over_mealy_fsm");
+	$dumpvars(0,tb_mealy_fsm);
+end
+endmodule
+
+
