a35090bfeb0c2a8d96742a321473acc514570245
Naveen <naveenmurugesan1972@gmail.com>
2026-03-02 12:05:04 +0530

    Add files via upload

---
+++ 3_Sequential/FSM/Sequence_detector/Mealy_FSM/1001_FSM/1001_overlapping.v
@@ -0,0 +1,53 @@
+module mealy_fsm(
+input clk,rst,a,
+output reg q);
+parameter [1:0] s0=2'b00,
+            s1=2'b01,
+            s2=2'b10,
+            s3=2'b11;
+reg [1:0]state,next_state;
+always@(posedge clk or posedge rst)begin
+state<=2'b00; 
+q<=1'b0;
+if(rst)
+state<=2'b00;
+else
+state<=next_state;
+end
+always@(*) begin
+case(state)
+s0:begin	
+if(a)
+next_state<=s1;
+else
+next_state<=s0;
+end
+s1:begin
+if(a)
+next_state<=s1;
+else
+next_state<=s2;
+end
+s2:begin
+if(a)
+next_state<=s1;
+else	
+next_state<=s3;
+end
+s3:begin
+if(a)begin
+next_state<=s1;
+q<=1'b1;
+end
+else 
+next_state<=s0;
+end
+endcase
+end
+endmodule
+
+		
+
+		   
+
+
---
+++ 3_Sequential/FSM/Sequence_detector/Mealy_FSM/1001_FSM/tb_1001_nonoverlapping.v
@@ -0,0 +1,27 @@
+module tb_mealy_fsm;
+reg clk,rst,a;
+wire q;
+mealy_fsm uut(.clk(clk), .rst(rst), .a(a), .q(q));
+always #5 clk=~clk;
+initial begin
+	clk=0; rst=1; a=0;
+	#5 rst=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=0;
+	#10 a=1;
+	#10 a=1;
+	#10 a=0;
+	#10 a=0;
+        #10 a=0;
+	#10 $finish;
+end
+initial begin
+	$monitor("Time=%0t | clk=%b | rst=%b | a=%b | q=%b",$time,clk,rst,a,q);
+	$dumpfile("nonover_mealy_fsm");
+	$dumpvars(0,tb_mealy_fsm);
+end
+endmodule
+
+
