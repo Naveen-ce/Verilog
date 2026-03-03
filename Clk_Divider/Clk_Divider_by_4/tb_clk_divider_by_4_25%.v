fbeb221345f1d04d03c396fd04d46727db155f2c
Naveen <naveenmurugesan1972@gmail.com>
2026-03-03 19:28:23 +0530

    Add files via upload

---
+++ Clk_Divider/Clk_Divider_by_4/tb_clk_divider_by_4_25%.v
@@ -0,0 +1,18 @@
+module tb_clk_divider_by_4;
+reg clk,rst;
+wire f_by_4;
+clk_divider_by_4 uut(.clk(clk), .rst(rst), .f_by_4(f_by_4));
+always #5 clk=~clk;
+initial begin
+clk=0; rst=1;
+#5 rst=0;
+#50 $finish;
+end
+initial begin
+$monitor("Time=%0t | clk=%b | rst=%b | f_by_4=%b",$time,clk,rst,f_by_4);
+$dumpfile("clk_divider_by_4_25%.vcd");
+$dumpvars(0,tb_clk_divider_by_4);
+end
+endmodule
+
+
