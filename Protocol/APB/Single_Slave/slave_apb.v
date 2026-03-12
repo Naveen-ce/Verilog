ec63e45800a483bc59b401905499f652ab6b6333
Naveen <naveenmurugesan1972@gmail.com>
2026-03-12 10:00:41 +0530

    Add files via upload

---
+++ Protocol/APB/Single_Slave/slave_apb.v
@@ -0,0 +1,36 @@
+module slave_apb #(parameter width=8,depth=64)
+(input pclk,
+input pwrite,
+input rst,
+input psel1,
+input penable,
+input [width-1:0] paddr,
+input [width-1:0] pwdata,
+output reg  pready,
+output reg [width-1:0] prdata);
+
+reg [width-1:0] ram[depth-1:0];
+integer i;
+
+always@(posedge pclk or posedge rst) begin
+if(rst) begin
+pready<=1;
+prdata<=0;
+ for(i=0;i<64;i=i+1)
+ ram[i]<=0;
+ end
+ else begin
+ pready<=1;
+if(psel1 && penable) begin
+
+	if(pwrite) 
+            ram[paddr]<=pwdata;
+
+    else
+	    prdata<=ram[paddr];
+  
+    end
+    end
+    end
+    endmodule
+
